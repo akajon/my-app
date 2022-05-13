fe9a87f02072dd35e8fc024056f04e8e3924450e
Anca Petrean <petrean.anca@yahoo.com>
2022-05-13 14:45:29 +0300

    added log in page added register page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,10 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
-
+import { NavigationComponent } from './navigation/navigation.component';
 const routes: Routes = [
-{ path: ' ', component: HomeComponent},
+{ path: '', component: HomeComponent},
+{ path: 'navigation', component: NavigationComponent},
  { path: 'login', component: LoginComponent},
  { path: 'register', component: RegisterComponent},
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { NavigationComponent } from './navigation/navigation.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
@@ -13,10 +16,15 @@ import { NavigationComponent } from './navigation/navigation.component';
     LoginComponent,
     HomeComponent,
     RegisterComponent,
-    NavigationComponent
+    NavigationComponent,
+
   ],
   imports: [
-    BrowserModule
+    AppRoutingModule,
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    //HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
